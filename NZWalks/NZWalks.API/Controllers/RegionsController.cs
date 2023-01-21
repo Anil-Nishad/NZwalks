18efeab6b9414d03105ef9d98b28e6ff7f360d94
AnilK <delhi2013guddu@gmail.com>
2023-01-21 17:09:17 +0530

    First GET Method

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Repository;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    // or [Route("Region")]
+    public class RegionsController : Controller
+    {
+        private readonly IRegionRepository regionRepository;
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public RegionsController(IRegionRepository regionRepository)
+        {
+            this.regionRepository = regionRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllRegions() 
+        {
+            var regions =  regionRepository.GetAll();
+
+           //var regions = new List<Region>()
+           //{ 
+           //    new Region
+           //    {
+           //         Id = Guid.NewGuid(),
+           //         Name= "Wellington",
+           //         Code = "WLG",
+           //         Area = 227755,
+           //         Lat = -1.8822,
+           //         Long = 299.88,
+           //         Population = 500000
+           //    },
+           //    new Region
+           //    {
+           //         Id = Guid.NewGuid(),
+           //         Name= "Auckland",
+           //         Code = "AUCK",
+           //         Area = 227755,
+           //         Lat = -1.8822,
+           //         Long = 299.88,
+           //         Population = 500000
+           //    }
+           //};
+            return Ok(regions);
+        }
+    }
+}
---
+++ NZWalks/NZWalks.API/Repository/RegionRepositiry.cs
@@ -0,0 +1,20 @@
+﻿using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repository
+{
+    public class RegionRepositiry : IRegionRepository
+    {
+        private readonly NZWalksDbContext nZWalksDbContext;
+
+        public RegionRepositiry(NZWalksDbContext nZWalksDbContext)
+        {
+            this.nZWalksDbContext = nZWalksDbContext;
+        }
+        public IEnumerable<Region> GetAll()
+        {
+            // throw new NotImplementedException();
+            return nZWalksDbContext.Regions.ToList();
+        }
+    }
+}
